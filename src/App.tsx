4b1b5a0534050525c1346e9d2341e83f38d701a3
ManasaNagireddy <manasa.bandapalli@gmail.com>
2025-04-16 11:20:35 +0530

    React Basic Step & List items Display

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import Message from './Message';
+import OrderList from './components/OrderList';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Message/>
+        <OrderList/>
+              </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/OrderList.tsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+function OrderList() {
+  return (
+    <Fragment>
+      <h2>Order List</h2>
+      <ul className="list-group">
+        <li className="list-group-item">Order 1</li>
+        <li className="list-group-item">Order 2</li>
+        <li className="list-group-item">Order 3</li>
+        <li className="list-group-item">Order 4</li>
+        <li className="list-group-item">Order 5</li>
+        <li className="list-group-item">Order 6</li>
+        <li className="list-group-item">Order 7</li>
+        <li className="list-group-item">Order 8</li>
+        <li className="list-group-item">Order 9</li>
+        <li className="list-group-item">Order 10</li>
+      </ul>
+      {/* Order list implementation */}
+    </Fragment>
+  );
+}
+export default OrderList;
